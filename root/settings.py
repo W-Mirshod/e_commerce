c92c3e9e9e1daab582698d336eb7caa7a0abfc9e
Mirshod <python3526@gmail.com>
2024-07-21 01:06:00 +0500

    Twitter and Facebook registering lore is done

--- root/settings.py
+++ root/settings.py
@@ -71,6 +71,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -143,10 +145,18 @@ EMAIL_HOST_PASSWORD = env('E_H_PASSWORD')
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'social_core.backends.google.GoogleOAuth2',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.twitter.TwitterOAuth',
 ]
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env('G_OAUTH2_KEY')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env('G_OAUTH2_SECRET')
+
+SOCIAL_AUTH_FACEBOOK_KEY = env('S_A_FACEBOOK_KEY')
+SOCIAL_AUTH_FACEBOOK_SECRET = env('S_A_FACEBOOK_SECRET')
+
+SOCIAL_AUTH_TWITTER_KEY = env('S_A_TWITTER_KEY')
+SOCIAL_AUTH_TWITTER_SECRET = env('S_A_TWITTER_SECRET')
 
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
@@ -161,3 +171,4 @@ SOCIAL_AUTH_PIPELINE = (
 )
 
 LOGIN_URL = 'http://127.0.0.1:8000/'
+LOGIN_REDIRECT_URL = 'http://127.0.0.1:8000/login/'
