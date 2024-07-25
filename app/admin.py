77d281c24eca0657744a43827c6e3e713a025c14
Mirshod <python3526@gmail.com>
2024-07-25 22:36:35 +0500

    some improvements

--- app/admin.py
+++ app/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 
 from app.models import Product, Images, AttributeKey, AttributeValue, AttributeReference
 
+
 # Register your models here.
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'discount', 'favoured')
+    search_fields = ('name',)
+    list_filter = ('id', 'price')
+
+
 admin.site.register(Images)
 admin.site.register(AttributeKey)
 admin.site.register(AttributeValue)
