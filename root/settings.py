75d2665eb00d0c322a989aff40efa613e37ae41b
Mirshod <python3526@gmail.com>
2024-07-25 20:07:20 +0500

    settings.py improved

--- root/settings.py
+++ root/settings.py
@@ -13,9 +13,7 @@ from pathlib import Path
 import environ
 import os
 
-env = environ.Env(
-    DEBUG=(bool, False)
-)
+env = environ.Env(DEBUG=(bool, False))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
